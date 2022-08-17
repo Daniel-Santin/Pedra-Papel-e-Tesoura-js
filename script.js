a3843a41266cf6cb4874febd2a9ca950d1615a3e
Daniel-Santin <32037139@mackenzista.com.br>
2022-08-17 16:17:53 -0300

    vapo

--- script.js
+++ script.js
@@ -7,17 +7,30 @@ let statusDisplay = document.querySelector("#status-head");
 let moveDisplays = document.querySelectorAll(".move-display h2");
 let buttons = document.querySelectorAll("button");
 
-/*
+/**
  * Computes result of the game. returns victory message if move 1 wins.
- * @param  {Number}   move1  movimento 1
- * @param  {Number}   move2  movimento 2
- * @return {String}   result resultado do game
-*/
+ * @param  {Number}   move1  move 1
+ * @param  {Number}   move2  move 2
+ * @return {String}   result result of the game
+ */
+function calcResult(move1, move2) {
 
-function calcResult(move1, move2) {}
+  // se pedra(0) - tesoura(2) = -2, papel(1)-pedra(0) = 1, tesour(2) - papel (1) =1.
+  // pedra(0) - papel(1) = -1, papel(1) - tesoura(2)= -1, tesoura(2) -pedra(0)=2. 
+  // outros resultados são empates pedra-pedra,papel-papel,tesoura-tesoura.
 
-/*
- * @return {Number}   retorna numero aleatorio entre 0 e 2 para escolher pedra,papel ou tesoura.
+  var result = move1 - move2;
+
+  if (result == 1 || result + 3 == 1) { 
+    return winMsg;
+  } else if (result == 2 || result + 3 == 2) { 
+    return loseMsg;
+  } else { 
+    return tieMsg;
+  }
+}
+/** 
+  @return {Number}
 */
 
 function randomMove() {
@@ -33,6 +46,8 @@ function startGame(){
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].textContent = moveList[i];
     buttons[i].style.display = "inline-block";
+    buttons[i].removeEventListener("click", startGame)
+    buttons[i].addEventListener("click", endGame);
   }
   
   moveDisplays.forEach(moveDisplay => moveDisplay.style.display="none");
@@ -43,6 +58,28 @@ function startGame(){
  * @param {Event} event event containing information of users input.
 */
 
-function endGame(event) {}
+
+function endGame(event) {
+  let playerMove = moveList.indexOf(event.target.textContent);
+  let computerMove = randomMove();
+
+  statusDisplay.textContent = calcResult(playerMove, computerMove);
+
+  moveDisplays.forEach(
+    (moveDisplay) => (moveDisplay.style.display = "inline-block")
+  );
+  moveDisplays[0].textContent = `You played ${moveList[playerMove]}`;
+  moveDisplays[1].textContent = `Computer played ${moveList[computerMove]}`;
+
+  buttons.forEach((button, index) => {
+    if (index == 1) {
+      button.textContent = "Play Again";
+      button.removeEventListener("click", endGame);
+      button.addEventListener("click", startGame);
+    } else {
+      button.style.display = "none";
+    }
+  });
+}
 
 startGame();
